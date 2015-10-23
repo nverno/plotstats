51074ba5a55f027f76545abb76520dc819124118
Noah Peart <noah.v.peart@gmail.com>
2015-10-23 01:20:04 -0400

    blah

--- app/controllers/subset.R
+++ app/controllers/subset.R
@@ -3,7 +3,7 @@
 ## Description: Controllers for partials/subset_ui.R
 ## Author: Noah Peart
 ## Created: Thu Oct 22 19:25:37 2015 (-0400)
-## Last-Updated: Fri Oct 23 00:12:13 2015 (-0400)
+## Last-Updated: Fri Oct 23 01:19:50 2015 (-0400)
 ##           By: Noah Peart
 ######################################################################
 ## Prefix: ''
@@ -18,20 +18,4 @@ dat <- reactive({
     } else blankDF(pp, tp)
     lb$set_keys(seq_len(nrow(res)))
     res
-})
-
-output$dataTable <- DT::renderDataTable(expr={
-    caption <- isolate({
-        if (is.null(input$data)) {
-            "No data selected"
-        } else if (input$data == 'tp') {
-            "Selected Transect Data"
-        } else if (input$data == 'pp') {
-            "Selected Permanent Plot Data"
-        } else "No data selected"
-    })
-    tabdat <- dat()
-    DT::datatable(tabdat, caption=caption, filter='bottom', rownames=FALSE,
-                  class='cell-border stripe',
-                  options=list(pageLength=5, autoWidth=TRUE))
 })
